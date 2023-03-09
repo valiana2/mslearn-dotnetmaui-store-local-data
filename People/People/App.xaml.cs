d9c8fdcbf70087fd8f8c9c815606bda175515318
Lisa Boulier <lisa.bourlier@epita.fr>
2023-03-09 12:45:01 +0700

    finished

--- People/People/App.xaml.cs
+++ People/People/App.xaml.cs
@@ -2,15 +2,14 @@
 
 public partial class App : Application
 {
-    // TODO: Add a public static PersonRepository property
+    public static PersonRepository PersonRepo { get; private set; }
 
-    public App()
-	{
-		InitializeComponent();
+    public App(PersonRepository repo)
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
+        MainPage = new AppShell();
 
-        // TODO: Initialize the PersonRepository property with the PersonRespository singleton object
-
+        PersonRepo = repo;
     }
 }
--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using People.Models;
 
 namespace People
 {
@@ -13,10 +15,16 @@ namespace People
         public string StatusMessage { get; set; }
 
         // TODO: Add variable for the SQLite connection
+        private SQLiteAsyncConnection conn;
 
-        private void Init()
+        private async Task Init()
         {
-            // TODO: Add code to initialize the repository         
+            if (conn != null)
+                return;
+
+            conn = new SQLiteAsyncConnection(_dbPath);
+
+            await conn.CreateTableAsync<Person>();
         }
 
         public PersonRepository(string dbPath)
@@ -24,35 +32,35 @@ namespace People
             _dbPath = dbPath;                        
         }
 
-        public void AddNewPerson(string name)
-        {            
+
+        public async Task AddNewPerson(string name)
+        {
             int result = 0;
             try
             {
-                // TODO: Call Init()
+                // Call Init()
+                await Init();
 
                 // basic validation to ensure a name was entered
                 if (string.IsNullOrEmpty(name))
                     throw new Exception("Valid name required");
 
-                // TODO: Insert the new person into the database
-                result = 0;
+                result = await conn.InsertAsync(new Person { Name = name });
 
-                StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+                StatusMessage = string.Format("{0} record(s) added [Name: {1})", result, name);
             }
             catch (Exception ex)
             {
                 StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
             }
-
         }
 
-        public List<Person> GetAllPeople()
+        public async Task<List<Person>> GetAllPeople()
         {
-            // TODO: Init then retrieve a list of Person objects from the database into a list
             try
             {
-                
+                await Init();
+                return await conn.Table<Person>().ToListAsync();
             }
             catch (Exception ex)
             {
